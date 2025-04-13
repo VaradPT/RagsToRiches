79c35d6da26f3ed557e109a8b469d9f8aa1b1447
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-04-13 22:44:05 +0530

    Update rootsy.hpp

--- Root finding/rootsy.hpp
+++ Root finding/rootsy.hpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 //Newton Raphson
-/* */
+/* Method under standing and program under construction :) */
 //Bisection
-//Iteration
+/*Given interval for a point... function, accepting the equation from user and giving useful results,  */
+//Fixed point
 
 #endif 
