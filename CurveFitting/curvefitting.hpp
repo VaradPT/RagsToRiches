d8da45311ecd9a4b0562f3fa83f86ed2583834ef
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-04-15 23:39:49 +0530

    Add files via upload

---
+++ CurveFitting/curvefitting.hpp
@@ -0,0 +1,6 @@
+//Least Square(Line, Parabola,Exponential, Powerfit), Extrapolation, Lagranges, Spline, Tschebyshev, Fourier
+
+#ifndef Curvefitting
+#define Curvefitting
+
+#endif
---
+++ CurveFitting/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "curvefitting.hpp"
+int main
+{
+    return 0;
+}
