13209e915b8c8ae9c96af5390304b1c09162b905
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-04-18 00:42:32 +0530

    Update complexClass.hpp

--- Complex/complexClass.hpp
+++ Complex/complexClass.hpp
@@ -4,10 +4,14 @@ class complexClass
 {
     private:
     public:
-}
+
 /*
 Complex number= a+ib (a,b are real and i is imaginary sqrt(-1))
 Same standards: Insertion, extractions, file handling etc .. Norm and complex conjugate finding
 */
-
+//insertion, extraction
+void accept();
+void display();
+void operations();
+}
 #endif
