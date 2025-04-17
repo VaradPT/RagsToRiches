aabc2a7c742983f9b7861473c84f86d3c9f41275
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-04-17 23:46:54 +0530

    Update wlcm.cpp

--- wlcm.cpp
+++ wlcm.cpp
@@ -5,6 +5,9 @@ int main()
 {
     cout<<"Hello, World!"<<endl;
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Various Header files are used for operations" << std::endl;
+    std::cout << "<iostream>: Basic IO <fstream>: File Ops etc " << std::endl;
+    
     return 0;
 }
 //This is just the basic code for printing
