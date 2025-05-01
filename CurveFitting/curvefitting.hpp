99bf25b21a9cc94a20d6977615c12ae73ea8bed7
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-05-01 23:35:14 +0530

    Update curvefitting.hpp

--- CurveFitting/curvefitting.hpp
+++ CurveFitting/curvefitting.hpp
@@ -1,3 +1,4 @@
+
 //Least Square(Line, Parabola,Exponential, Powerfit), Extrapolation, Lagranges, Spline, Tschebyshev, Fourier
 
 #ifndef Curvefitting
@@ -6,9 +7,15 @@ class Curvefittin
 {
 public:
   void leastSq();
+//working
+  void curvefit();
+//various 
   void lagranges();
+//extrapolation: number of points
   void spline();
+//understand that
   void tschebyshev();
+//revise that 
 
 }
 #endif
