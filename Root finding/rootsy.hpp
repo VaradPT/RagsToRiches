a5de7fe8373fa82792d7dc3b83bf9e8360de175c
Varad Tapray <166056504+VaradPT@users.noreply.github.com>
2025-05-19 23:28:04 +0530

    Update rootsy.hpp

--- Root finding/rootsy.hpp
+++ Root finding/rootsy.hpp
@@ -4,7 +4,6 @@
 using namespace std;
 
 //Newton Raphson Method
-/* Method understanding and program under construction :) */
 void NewtonRaphson();
 //Bisection Method
 /*Given interval for a point... function, accepting the equation from user and giving useful results,  */
